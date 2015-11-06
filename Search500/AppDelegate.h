2dfa3c0e983824204bd86c1239aa71cb5c26e24a
offline <offline@nti31329am.local>
2015-11-06 08:50:04 +0000

    Initial Commit

---
+++ Search500/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Search500
+//
+//  Created by offline on 06/11/2015.
+//  Copyright © 2015 moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Search500/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Search500
+//
+//  Created by offline on 06/11/2015.
+//  Copyright © 2015 moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
